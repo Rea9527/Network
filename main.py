b440e0e4a84b7643cb251594851f11264e1fa6ef
wananren <wananren.l@gmail.com>
2018-12-06 18:03:03 +0800

    update outline

---
+++ main.py
@@ -0,0 +1,21 @@
+import networkx
+
+
+class Community:
+    graph = networkx.Graph()
+    betweenness_list = {}
+
+    def __inti__(self):
+        pass
+
+    def read_graph(self):
+        pass
+
+    def calcul_betw(self):
+        pass
+
+    def cut_edge(self):
+        pass
+
+    def evaluation(self):
+        pass
